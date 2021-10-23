4d6b7a052cd9d24f77649f579887edca9b70f1da
PHROMPONG KHAGTES <zphrompong.k@pttdigital.com>
2021-10-23 08:55:26 +0700

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+// Router imports
+// End of router imports
+
+const router = express.Router();
+
+// Route routers
+// End of routing routers
+
+export default router;
